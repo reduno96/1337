c5ffb7e41956cae51b6124fe706c789cdf7ddfe0
Redouane El MoRabet <115698395+reduno96@users.noreply.github.com>
2023-09-07 13:08:52 +0100

    add c03

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-mora <rel-mora@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 17:54:21 by rel-mora          #+#    #+#             */
+/*   Updated: 2023/08/26 15:08:04 by rel-mora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	j = 0;
+	i = 0;
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j] && j < nb)
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
